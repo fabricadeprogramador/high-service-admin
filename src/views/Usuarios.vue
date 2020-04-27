288b63bb72d9ac25438b8a628e6a30bb02de19f9
Galaraz <jvictor.si.2013@gmail.com>
2020-04-27 11:18:16 -0400

    Crud de Usuario

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -1,88 +1,252 @@
 <template>
-  <v-app id="formularioCadastroUsuario">
-    <v-content class="pa-0">
-      <v-container fluid>
-        <v-row justify="center" >
-          <v-col cols="12" sm="12" md="12">
-            <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Cadastro de Usuários</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form v-model="valid" ref="form">
-                  <v-text-field
-                    label="Nome de usuário (de 2 a 10 caracteres)"
-                    name="username"
-                    prepend-icon="mdi-account"
-                    type="text"
-                    v-model="usuario.username"
-                    :rules="nameRules"
-                  />
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="empresa"
+    class="elevation-1"
+     >
 
-                  <v-text-field
-                    id="password"
-                    label="Senha (6 caracteres)"
-                    name="password"
-                    prepend-icon="mdi-lock"
+
+    <template v-slot:top>
+
+
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.nome" :readonly="readonly"
+                    :rules="nomeRules" 
+                    :counter="25"
+                    label="Nome"
+                    required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+
+
+                    <v-text-field v-model="editedItem.senha" :readonly="readonly"
+                    :rules="senhaRules"
+                    :counter="25"
+                    label="Senha"
                     type="password"
-                    v-model="usuario.password"
-                    :rules="passwordRules"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions class="pb-5">
-                <v-spacer />
-                <v-btn :disabled="!valid" class="mr-3" color="primary" @click="cadastrarUsuario()">Salvar</v-btn>
-                <v-btn color="primary" @click="reset()">Cancelar</v-btn>
-                <v-spacer />
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+                    required></v-text-field>
+                  </v-col>
+                 
+           
+                  <v-col cols="12" sm="6" md="4">
+                    <v-select v-model="editedItem.papel"
+                    :items= "Papel"
+                    :rules= "[v => !!v || 'Selecione um status' ]"
+                    label= "Papel"
+                    required></v-select>
+                  </v-col>
+                </v-row>
+            </v-container>
+          </v-form>
+        </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              
+              <v-btn color="blue darken-1" text @click="save">Salvar</v-btn>
+
+            <v-btn color="blue darken-1" text @click="close">Cancelar</v-btn>
+            
+            </v-card-actions>
+          </v-card>
+        
+
+
+
+     
+    </template>
+    <template v-slot:item.acoes="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-magnify</v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      usuario: {},
-      usuarios: [{username: "teste", password: "testee"}],
-      valid: true,
-      nameRules: [
-        v => !!v || 'Digite um nome de usuário!',
-        v => (!!v && v.length >=2 && v.length <= 10) || 'Nome de usuário deve possuir no mínimo 2 e no máximo 10 caracteres!',
-      ],
-      passwordRules: [
-        v => !!v || 'Digite uma senha!',
-        v => (!!v && v.length == 6) || 'A senha deve possuir 6 caracteres!',
-      ],
+  export default {
+    data: () => ({
+     
+      modal: false,
+      dialog: false,
+      readonly: false,
+      headers: [
+         {
+            text: 'Usuarios',
+            align: 'start',
+            sortable: false,
+            value: 'nome',
+          },
+{ text: 'Papel', value: 'papel' },
 
-    };
-  },
-  methods: {
-    cadastrarUsuario() {
-      let usuario = Object.assign({}, this.usuario)
-      usuario.tipo = "ADMIN"
-      this.usuarios.push(usuario);
-      alert(
-        JSON.stringify(this.usuarios)
-      );
-      // *** Nessa linha faz o request para a API salvar o novo usuário
-      this.reset()
+{ text: 'Ações', value: 'acoes', sortable: false },
+],
+      
+
+      papel: ['Funcional','Adm'],
+      desserts: [],
+      editedIndex: -1,
+      
+      editedItem: {
+        name: '',
+        senha: '',
+        papel: '',
+      },
+
+      defaultItem: {
+        name: '',
+        senha:'',
+        papel: '',
+        
+      },
+
+        nomeRules: [
+          v=> !!v || 'Nome é Obrigatório',
+          v=> v.length <= 25 || 'Requer menos de 25 caracteres'
+        ],
+
+        senhaRules: [
+          v=> !!v || 'Senha é Obrigatório',
+          v=> v.length <= 25 || 'Requer menos de 25 caracteres'
+        ],
+
+        papelRules: [
+          v=> !!v || 'Valor Obrigatório'
+        ],
+
+        
+
+    }),
+
+  computed: {
+formTitle () {
+return this.editedIndex === -1 ? 'Novo Usuario' : 'Edite Usario'
+},
+},
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+     
     },
-    
-    reset() {
-      this.$refs.form.reset()
-    }
-  }
-};
+
+ created () {
+this.initialize()
+},
+
+    methods: {
+initialize () {
+this.desserts = [
+{
+nome: 'JAO',
+papel: 'Adm',
+
+
+},
+{
+nome: 'JAO',
+papel: 'Funcional',
+
+
+},
+{
+nome: 'JAO',
+papel: 'Adm',
+
+
+},
+{
+nome: 'JAO',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Jones',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Jhonne',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Jhon',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Ruan',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Luan',
+papel: 'Adm',
+
+
+},
+{
+nome: 'Juan',
+papel: 'Adm',
+
+
+},
+]
+},
+
+editItem (item) {
+this.editedIndex = this.desserts.indexOf(item)
+this.editedItem = Object.assign({}, item)
+this.dialog = true
+},
+
+deleteItem (item) {
+const index = this.desserts.indexOf(item)
+confirm('Deseja realmente deletar esse item') && this.desserts.splice(index, 1)
+},
+
+close () {
+this.dialog = false
+setTimeout(() => {
+this.editedItem = Object.assign({}, this.defaultItem)
+this.editedIndex = -1
+}, 300)
+},
+
+save () {
+if (this.editedIndex > -1) {
+Object.assign(this.desserts[this.editedIndex], this.editedItem)
+} else {
+this.desserts.push(this.editedItem)
+}
+this.close()
+},
+},
+}
 </script>
 
-<style>
-.v-btn {
-width: 100px;
-}
-</style>
+
